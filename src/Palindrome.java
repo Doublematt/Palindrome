932ce00dea9fa751cc6ec3672159a2342021a7ab
Mateusz Matusewicz <mateusz.matusewicz16@gmail.com>
2022-03-28 17:22:24 +0200

    new method in Polindrome - getReversedWord

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -6,16 +6,22 @@ public class Palindrome {
 	public static boolean isPalindrome(String word) {
 		
 		word = word.toLowerCase();
-		String reverse = "";
-		for(int i = word.length()-1; i>= 0; i--) {	
-			reverse += word.charAt(i);			
-			
-		}
+		
+		String reverse = getReversedWord(word);
 		
 		if(word.equals(reverse)) return true;
 		
 		return false;
 		
 		
+	}
+	
+	public static String getReversedWord (String word) {
+		
+		String reverse = "";
+		for(int i = word.length()-1; i>= 0; i--) 	
+			reverse += word.charAt(i);			
+			
+		return reverse;
 	}
 }
